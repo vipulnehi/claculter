a2e3acd5523b415bd477c2a103f46eec2929325c
Vipul negi <158534215+vipulnehi@users.noreply.github.com>
2024-04-03 08:40:52 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,63 @@
+
+// // variable
+// console.log("vipul is fullstak devloer");
+
+// fullName = "vipul";
+// console.log(fullName);
+// console.log("fullname");
+// x = null;
+// console.log(x);
+// y = undefined;
+// console.log(y);
+// // bullion valur 
+// // this types of value is always store true or false type of values
+// isfollow = false;
+// console.log(isfollow);
+// isfollow = true;
+// console.log(isfollow);
+
+// // it is dynamic type laguage 
+// fullnamem= "vipul negi";
+
+// // we can see this is language is ase sesetive laguage
+// const pi = 3.14;
+// console.log(pi);d
+// let U=BigInt("1234");
+
+
+// Object is a collection of values for example  age cgpa fullName 
+const student={
+    fullNAme:"Rahul Kumar",
+    age:20,
+    cgpa:8.2,
+    isPass:true,
+
+};
+student["age"]=student["age"]+1;
+
+console.log(student.age);
+//   that is the way how to change the valaue in js 
+let a=25;
+a=10;
+// how to change the name or other words we already assined
+student["fullNAme"]="vipul negi";
+console.log(student["fullNAme"]);
+
+// home work of first class
+const product={
+    tittle: "pine apple",
+    price: 230,
+    rating: 4,
+    offer: 5,
+
+}
+console.log(product);
+const profile={
+    userName:"@cipulneg_i",
+    followers:50000000,
+    following:500,
+    isFollow: false,
+
+};
+console.log( typeof profile["isFollow"]);
+console.log(typeof profile["userName"])
